3104d6b58679bba98d20ef2e02915313c27727d2
gabriellebyers <gabriellembyers@gmail.com>
2020-07-09 13:59:05 -0500

    added handlebars

--- server.js
+++ server.js
@@ -1,43 +1,125 @@
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-
-mongoose.connect(MONGODB_URI);
 
 
-var cheerio = require("cheerio");
+
+
+
+var express = require("express");
+var mongoose = require("mongoose");
+var handlebars = require("express-handlebars")
 var axios = require("axios");
+var cheerio = require("cheerio");
 
-// First, tell the console what server3.js is doing
-console.log("\n******************************************\n" +
-            "Look at the image of every award winner in \n" +
-            "one of the pages of `awwwards.com`. Then,\n" +
-            "grab the image's source URL." +
-            "\n******************************************\n");
+// Require all models
+var db = require("./models");
 
-// Make request via axios to grab the HTML from `awwards's` clean website section
-axios.get("http://www.awwwards.com/websites/clean/").then(function(response) {
+var PORT = process.env.PORT || 3003;
 
-  // Load the HTML into cheerio
-  var $ = cheerio.load(response.data);
+// Initialize Express
+var app = express();
 
-  // Make an empty array for saving our scraped info
-  var results = [];
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+// Connect to the Mongo DB
+mongoose.connect(MONGODB_URI);
+// Routes
 
-  // With cheerio, look at each award-winning site, enclosed in "figure" tags with the class name "site"
-  $("figure.rollover").each(function(i, element) {
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
 
-    /* Cheerio's find method will "find" the first matching child element in a parent.
-     *    We start at the current element, then "find" its first child a-tag.
-     *    Then, we "find" the lone child img-tag in that a-tag.
-     *    Then, .attr grabs the imgs srcset value.
-     *    The srcset value is used instead of src in this case because of how they're displaying the images
-     *    Visit the website and inspect the DOM if there's any confusion
-    */
-    var imgLink = $(element).find("a").find("img").attr("data-srcset").split(",")[0].split(" ")[0];
 
-    // Push the image's URL (saved to the imgLink var) into the results array
-    results.push({ link: imgLink });
+
+
+// A GET route for scraping the echoJS website
+app.get("/scrape", function(req, res) {
+  // First, we grab the body of the html with axios
+  axios.get("http://www.kxan.com/").then(function(response) {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(response.data);
+    
+
+
+    // Now, we grab every h2 within an article tag, and do the following:
+    $("article h3").each(function(i, element) {
+      // Save an empty result object
+      var result = {};
+
+      // Add the text and href of every link, and save them as properties of the result object
+      result.title = $(this)
+        .children("a")
+        .text();
+      result.link = $(this)
+        .children("a")
+        .attr("href");
+
+      // Create a new Article using the `result` object built from scraping
+      db.Article.create(result)
+        .then(function(dbArticle) {
+          // View the added result in the console
+          console.log(dbArticle);
+        })
+        .catch(function(err) {
+          // If an error occurred, log it
+          console.log(err);
+        });
+    });
+
+    // Send a message to the client
+    res.send("Scrape Complete");
   });
+});
 
-  // After looping through each element found, log the results to the console
-  console.log(results);
+// Route for getting all Articles from the db
+app.get("/articles", function(req, res) {
+  // Grab every document in the Articles collection
+  db.Article.find({})
+    .then(function(dbArticle) {
+      // If we were able to successfully find Articles, send them back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+// Route for grabbing a specific Article by id, populate it with it's note
+app.get("/articles/:id", function(req, res) {
+  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+  db.Article.findOne({ _id: req.params.id })
+    // ..and populate all of the notes associated with it
+    .populate("note")
+    .then(function(dbArticle) {
+      // If we were able to successfully find an Article with the given id, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+// Route for saving/updating an Article's associated Note
+app.post("/articles/:id", function(req, res) {
+  // Create a new note and pass the req.body to the entry
+  db.Note.create(req.body)
+    .then(function(dbNote) {
+      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+    })
+    .then(function(dbArticle) {
+      // If we were able to successfully update an Article, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+// Start the server
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
 });
